50d9922665f3d98906962d3ebce42919aa97eec2
okmanl <ericgumba@gmail.com>
2017-04-25 12:41:33 -0700

    increased readability

--- gameObjects/TankWorldEvents.java
+++ gameObjects/TankWorldEvents.java
@@ -7,8 +7,8 @@ import java.awt.event.KeyEvent;
  * Created by ericgumba on 4/22/17.
  */
 public class TankWorldEvents extends Observable {
-  public int eventType; // 1 = user input,  2 = in-game events, 3 = timed events
-  public Object event;  //this is made an object so it can be int, string etc
+  public int eventType;
+  public Object event;
 
   public void setValue(KeyEvent k, int keyEventType) {
 
@@ -18,7 +18,7 @@ public class TankWorldEvents extends Observable {
     try {
       notifyObservers(this);
     } catch (Exception e){
-      System.out.println("I wish I majored in math");
+      System.out.println("Wrong button was pressed.");
     }
   }
 }
