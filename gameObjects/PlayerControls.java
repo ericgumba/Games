86c644b1265334bdbb56a61c34755296ce053dd5
okmanl <ericgumba@gmail.com>
2017-04-25 12:25:36 -0700

    increased readability

--- gameObjects/PlayerControls.java
+++ gameObjects/PlayerControls.java
@@ -10,15 +10,15 @@ public class PlayerControls extends KeyAdapter {
 
   TankWorldEvents tankWorldEvents;
 
-  public PlayerControls(TankWorldEvents tankWorldEvents) {
+  public PlayerControls( TankWorldEvents tankWorldEvents ) {
     this.tankWorldEvents = tankWorldEvents;
   }
 
-  public void keyPressed(KeyEvent p) {
-    tankWorldEvents.setValue(p, 1);
+  public void keyPressed( KeyEvent pressed ) {
+    tankWorldEvents.setValue(pressed, 1);
   }
 
-  public void keyReleased(KeyEvent r) {
-    tankWorldEvents.setValue(r, 0);
+  public void keyReleased( KeyEvent released ) {
+    tankWorldEvents.setValue( released, 0 );
   }
 }
